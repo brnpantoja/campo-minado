8f94b35ddb08f55d5c297865f7f6ac14f0c43663
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-10-01 13:12:31 -0300

    Atributos de movimentação na classe campo

--- src/br/com/pantoja/cm/modelo/Campo.java
+++ src/br/com/pantoja/cm/modelo/Campo.java
@@ -1,5 +1,7 @@
 package br.com.pantoja.cm.modelo;
 
+import br.com.pantoja.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +20,7 @@ public class Campo {
         this.coluna = coluna;
     }
 
-    public boolean adicionarVizinho(Campo vizinho){
+    public boolean adicionarVizinho(Campo vizinho) {
         boolean linhaDiferente = linha != vizinho.linha;
         boolean colunaDiferente = coluna != vizinho.coluna;
         boolean diagonal = linhaDiferente && colunaDiferente;
@@ -27,10 +29,10 @@ public class Campo {
         int deltaColuna = Math.abs(coluna - vizinho.coluna);
         int deltaGeral = deltaColuna + deltaLinha;
 
-        if(deltaGeral == 1 && !diagonal){
+        if (deltaGeral == 1 && !diagonal) {
             vizinhos.add(vizinho);
             return true;
-        } else if(deltaGeral == 2 && diagonal){
+        } else if (deltaGeral == 2 && diagonal) {
             vizinhos.add(vizinho);
             return true;
         } else {
@@ -38,4 +40,37 @@ public class Campo {
         }
 
     }
+
+    void alternarMarcacao() {
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir() {
+        if (!aberto && !marcado) {
+            aberto = true;
+            if (minado) {
+                throw new ExplosaoException();
+            }
+
+            if (vizinhancaSegura()) {
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+        } else {
+
+            return false;
+        }
+
+    }
+
+    boolean vizinhancaSegura() {
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
+    void minar() {
+        minado = true;
+    }
 }
+
