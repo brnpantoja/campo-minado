c1332ef79d83d7d5840f725f1bb3c7d95c0ecf70
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-10-01 12:56:38 -0300

    Teste de integração classe campo

--- src/br/com/pantoja/cm/modelo/Tabuleiro.java
+++ src/br/com/pantoja/cm/modelo/Tabuleiro.java
@@ -1,4 +1,13 @@
 package br.com.pantoja.cm.modelo;
 
 public class Tabuleiro {
+    private final int linhas;
+    private final int colunas;
+    private final int minas;
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+    }
 }
